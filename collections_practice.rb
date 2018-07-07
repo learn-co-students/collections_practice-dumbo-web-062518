afa602846d8c7958780455eb7ebcead6010e71b5
Dan Chung <danetix@gmail.com>
2018-07-07 15:33:15 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |x, y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |element|
+    element[2]= "$"
+    element
+  end
+end
+
+def find_a(array)
+  array.select { |element| element[0].include? "a" }
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |element|
+    sum += element
+  end
+  sum
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+      element + "s"
+    end
+  end
+end
